ef3ffd88a82a228694716c612631a7d82ef0818f
manthri-kumar <153302220+manthri-kumar@users.noreply.github.com>
2025-11-11 21:01:26 +0530

    Pulled updated assets for Medicines page from Mohith branch

--- my-web-app/src/assets/index.js
+++ my-web-app/src/assets/index.js
@@ -17,23 +17,105 @@ import Nutritiousimg from "./nutritious.jpg";
 import summerimg from "./summer.jpg"
 import Docimg from "./Doc.jpg"
 import ambulance from "./ambulance.jpg"
-import bloodbank from "./Bloodbank.jpg"
-import LaShield from "./La Shield Sunscreen Gel SPF 40, 50 g.jpg";
-import Minimalist from "./Minimalist.jpg";
-import Neutrogena from "./Neutrogena Deep Clean Foaming Cleanser .jpg";
-import Ordinary from  "./The Ordinary Hyaluronic.jpg";
-import  Avene from "./Avene Cleanance Cleansing Gel 200 ml.jpg";
-import carave from "./CeraVe Moisturising Lotion 473 ml.jpg";
-import himalaya from "./Himalaya Gentle Baby Shampoo 200ml.jpg";
-import johnson from "./Johnson's Baby Powder 100g.jpg";
-import pampers from "./Pampers Active Baby Diapers (M, 56 Pcs).jpg";
-import mamaearth from "./Mamaearth Moisturizing Baby Bathing Soa.jpg";
-import sebamed from "./Sebamed Baby Lotion 200ml.jpg";
-import dermadew from "./Dermadew Baby Soap 75g.jpg";
-import huggies from "./Huggies Wonder Pants Diapers (L, 44 Pcs).jpg";
+import dolo650 from "./dolo650.jpg"
+import Mankind_Manforce_Condoms from "./Mankind Manforce Condoms.png"
+import HealthKart_Vitamin_D3 from "./HealthKart Vitamin D3.png"
+import Carbimide_Forte_Vitamin_C from "./Carbamide Forte Vitamin C.png"
+import Optimum_Nutrition_Gold_Standard_Whey from "./Optimum Nutrition Gold Standard Whey.png"
+import Tynor_Wrist_Support from "./Tynor Wrist Wrap.png"
+import Dove_Intense_Repair_Shampoo from "./Dove Intense Repair Shampoo.png"
+import First_Aid_Compact_Kit from "./First Aid Compact Kit.png"
+import KY_Jelly from "./K-Y Jelly.png"
+import MuscleTech_Mass_Tech from "./MuscleTech Mass Tech.png"
+import SBL_Aconite_Napellus_30 from "./SBL Aconite Napellus 30.png"
+import Dr_ReckewegBC_16 from "./DrReckeweg BC 16.png"
+import Dr_Reckeweg_Nux_Vomica_200 from "./DrReckeweg Nux Vomica 200.png"
+import Durex_Air_Condoms from "./Durex Air Condoms.png"
+import Durex_Play_Lubricant from "./Durex Play Lubricant.png"
+import Dynaplast_Elastic_Adhesivev from "./Dynaplast Elastic Adhesivev.png"
+import Family_First_Aid_Box from "./Family First Aid Box.png"
+import Flamingo_Wrist_Support from "./Flamingo Wrist Support.png"
+import HealthKart_HK_Vitals_Multivitamin from "./HealthKart HK Vitals Multivitamin.png"
+import HealthKart_Vitamine_C from "./HealthKart Vitamin C.png"
+import HealthKart_Vitamine_D3 from "./HealthKart Vitamin D3.png"
+import Himalaya_Tentex_Royal from "./Himalaya Tentex Royal.png"
+import LOreal_Total_Repair_5_Conditioner from "./LOreal Total Repair 5 Conditioner.png"
+import Mamaearth_Onion_Shampoo from "./Mamaearth Onion Shampoo.png"
+import Mamaearth_Onion_Hair_Oil from "./Mamaearth Onion Hair Oil.png"
+import Musli_Power_Xtra from "./Musli Power X-tra.png"
+import MuscleTech_Platinum_Creatine from "./MuscleTech Platinum Creatine.png"
+import MyProtein_Impact_Whey_Protein from "./MyProtein Impact Whey.png"
+import SBL_Bio_Combination_28 from "./SBL Bio-Combination 28.png"
+import Sporlac_Probiotic_Capsules from "./Sporlac Probiotic Capsules.png"
+import Supradyn_Daily from "./Supradyn Daily.png"
+import TrueBasics_Omega3 from "./TrueBasics Omega-3.png"
+import Tynor_Shoulder_Support from "./Tynor Shoulder Support.png"
+import Vissco_Knee_Brace from "./Vissco Knee Brace.png"
+import VSL3_Probiotic from "./VSL3 Probiotic.png"
+import Wheezal_Thuja_Occidentalis_Q from "./Wheezal Thuja Occidentalis Q.png"
+import WOW_Apple_Cider_Vinegar_Conditioner from "./WOW Apple Cider Vinegar Conditioner.png"
+import WOW_Omega_3 from "./WOW Omega-3.png"
+import Cipladine_Providone_Iodine from "./Cipladine Povidone Iodine.png"
+import Calcirol_Sachet_Vitamine_D3 from "./Calcirol Sachet Vitamin D3.png"
+import Dabur_Chyawanprash from "./Dabur Chyawanprash.png"
+import BigMuscles_Real_Mass from "./BigMuscles Real Mass.png"
+import Baidhyanath_Chyawanprash from "./Baidhyanath Chyawanprash.png"
+import BandAid_Flexible_Fabric from "./Band-Aid Flexible Fabric.png"
+import BigMuscles_Micronized_Creatine from "./BigMuscles Micronized Creatine.png"
+import Dettol_Antiseptic_Liquid from "./Dettol Antiseptic Liquid.png"
+import Flamingo_Shoulder_Support from "./Flamingo Shoulder Support.png"
+import Himalayan_Organics_Vitamin_C from "./Himalayan Organics Vitamin C.png"
+import Parachute_Adavanced_Aloe_Vera_Oil from "./Parachute Advanced Aloe Vera Oil.png"
+import SBL_Belladonna_30 from "./SBL Belladonna 30.png"
+import AccuSure_Knee_Cap from "./AccuSure Knee Cap.png"
+import SBL_Arnica_Montana_Q from "./SBL Arnica Montana Q.png"
+import Cipla_Vitamine_D3 from "./Cipla Vitamin D3.png"
+import GNC_Pro_Performance_Creatine from "./GNC Pro Performance Creatine.png"
+import Indulekha_Bringha_Hair_Oil from "./Indulekha Bringha Hair Oil.png"
+import Kapiva_Shilajit_Gold from "./Kapiva Shilajit Gold.png"
+import Mamaearth_Onion_Conditioner from "./Mamaearth Onion Conditioner.png"
+import MuscleBlaze_Mass_Gainer from "./MuscleBlaze Mass Gainer.png"
+import MuscleBlaze_Whey_Protein from "./MuscleBlaze Whey Protein.png"
+import Revital_H_Multivitamin from "./Revital H Multivitamin.png"
+import Savlon_Antiseptic_Liquid from "./Savlon Antiseptic Liquid.png"
+import Scalpe_Plus_Anti_Dandruff_Shampoo from "./Scalpe Plus Anti-Dandruff Shampoo.png"
+import Skore_Cool_Lubricant from "./Skore Cool Lubricant.png"
+import Skore_Timeless_Condoms from "./Skore Timeless Condoms.png"
+import Tynor_Knee_Support from "./Tynor Knee Support.png"
+import Tynor_Wrist_Wrap from "./Tynor Wrist Wrap.png"
+import Vissco_Shoulder_Immobilizer from "./Vissco Shoulder Immobilizer.png"
+import Vizylac_Probiotic from "./Vizylac Probiotic.png"
+import Zandu_Chyawanprash_Avaleha from "./Zandu Chyawanprash Avaleha.png"
+import HK_Vitals_Fish_Oil from "./HK Vitals Fish Oil.png"
+import Hansaplast_Regular_Bandage from "./Hansaplast Regular Bandage.png"
+import Vissco_Wrist_Brace from "./Vissco Wrist Brace.png"
+import Travel_First_Aid_Pouch from "./Travel First Aid Pouch.png"
 
-
-export { sanjeevaniImg,offerimg,bannerimg,medicineimg,doctorimg,labimg,healthimg,monitorimg
-        ,babyimg,painimg,proteinimg,vitaminimg,skincareimg,ayurvedicimg,Nutritiousimg,summerimg,
-        Docimg,ambulance,bloodbank,LaShield,Minimalist,Neutrogena,Ordinary,Avene,carave,himalaya,johnson,pampers,
-        mamaearth,sebamed,dermadew,huggies };
+export {
+  sanjeevaniImg,offerimg,bannerimg,medicineimg,doctorimg,labimg,healthimg,
+  monitorimg,babyimg,painimg,proteinimg,vitaminimg,skincareimg,ayurvedicimg,
+  Nutritiousimg,summerimg,Docimg,ambulance,dolo650,Mankind_Manforce_Condoms,
+  HealthKart_Vitamin_D3,Carbimide_Forte_Vitamin_C,Optimum_Nutrition_Gold_Standard_Whey,
+  Tynor_Wrist_Support,Dove_Intense_Repair_Shampoo,First_Aid_Compact_Kit,KY_Jelly,
+  MuscleTech_Mass_Tech,SBL_Aconite_Napellus_30,Dr_ReckewegBC_16,Dr_Reckeweg_Nux_Vomica_200,
+  Durex_Air_Condoms,Durex_Play_Lubricant,Dynaplast_Elastic_Adhesivev,Family_First_Aid_Box,
+  Flamingo_Wrist_Support,HealthKart_HK_Vitals_Multivitamin,HealthKart_Vitamine_C,
+  HealthKart_Vitamine_D3,Himalaya_Tentex_Royal,LOreal_Total_Repair_5_Conditioner,
+  Mamaearth_Onion_Shampoo,Mamaearth_Onion_Hair_Oil,Musli_Power_Xtra,
+  MuscleTech_Platinum_Creatine,MyProtein_Impact_Whey_Protein,
+  SBL_Bio_Combination_28,Sporlac_Probiotic_Capsules,Supradyn_Daily,
+  TrueBasics_Omega3,Tynor_Shoulder_Support,Vissco_Knee_Brace,VSL3_Probiotic,
+  Wheezal_Thuja_Occidentalis_Q,WOW_Apple_Cider_Vinegar_Conditioner,WOW_Omega_3,
+  Cipladine_Providone_Iodine,Calcirol_Sachet_Vitamine_D3,Dabur_Chyawanprash,
+  BigMuscles_Real_Mass,Baidhyanath_Chyawanprash,BandAid_Flexible_Fabric,
+  BigMuscles_Micronized_Creatine,Dettol_Antiseptic_Liquid,Flamingo_Shoulder_Support,
+  Himalayan_Organics_Vitamin_C,Parachute_Adavanced_Aloe_Vera_Oil,SBL_Belladonna_30,
+  AccuSure_Knee_Cap,SBL_Arnica_Montana_Q,Cipla_Vitamine_D3,
+  GNC_Pro_Performance_Creatine,Indulekha_Bringha_Hair_Oil,Kapiva_Shilajit_Gold,
+  Mamaearth_Onion_Conditioner,MuscleBlaze_Mass_Gainer,MuscleBlaze_Whey_Protein,
+  Revital_H_Multivitamin,Savlon_Antiseptic_Liquid,
+  Scalpe_Plus_Anti_Dandruff_Shampoo,Skore_Cool_Lubricant,Skore_Timeless_Condoms,
+  Tynor_Knee_Support,Tynor_Wrist_Wrap,Vissco_Shoulder_Immobilizer,
+  Vizylac_Probiotic,Zandu_Chyawanprash_Avaleha,HK_Vitals_Fish_Oil,Hansaplast_Regular_Bandage,
+  Vissco_Wrist_Brace,Travel_First_Aid_Pouch,
+};

31678cca8b29d3e9efce6878216057b17f6befe9
ReddyEswarK126 <katarureddyeswar@gmail.com>
2025-11-13 16:42:10 +0530

    Added pictures for Health Moniter Page

--- my-web-app/src/assets/index.js
+++ my-web-app/src/assets/index.js
@@ -18,6 +18,52 @@ import summerimg from "./summer.jpg"
 import Docimg from "./Doc.jpg"
 import ambulance from "./ambulance.jpg"
 import bloodbank from "./Bloodbank.jpg"
+import h1 from "./h1.jpg"
+import h2 from "./h2.jpg"
+import h3 from "./h3.jpg"
+import h4 from "./h4.jpg"
+import h5 from "./h5.jpg"
+import h6 from "./h6.jpg"
+import h7 from "./h7.jpg"
+import h8 from "./h8.jpg"
+import h9 from "./h9.jpg"
+import h10 from "./h10.jpg"
+import h11 from "./h11.jpg"
+import h12 from "./h12.jpg"
+import h13 from "./h13.jpg"
+import h14 from "./h14.jpg"
+import h15 from "./h15.jpg"
+import h16 from "./h16.jpg"
+import p1 from "./p1.jpg"
+import p2 from "./p2.jpg"
+import p3 from "./p3.jpg"
+import p4 from "./p4.jpg"
+import p5 from "./p5.jpg"
+import p6 from "./p6.jpg"
+import p7 from "./p7.jpg"
+import p8 from "./p8.jpg"
+import p9 from "./p9.jpg"
+import p10 from "./p10.jpg"
+import b1 from "./b1.jpg"
+import b2 from "./b2.jpg"
+import b3 from "./b3.jpg"
+import b4 from "./b4.jpg"
+import b5 from "./b5.jpg"
+import b6 from "./b6.jpg"
+import b7 from "./b7.jpg"
+import b8 from "./b8.jpg"
+import b9 from "./b9.jpg"
+import b10 from "./b10.jpg"
+import a1 from "./a1.jpg"
+import a2 from "./a2.jpg"
+import a3 from "./a3.jpg"
+import a4 from "./a4.jpg"
+import a5 from "./a5.jpg"
+import a6 from "./a6.jpg"
+import a7 from "./a7.jpg"
+import a8 from "./a8.jpg"
+import a9 from "./a9.jpg"
+import a10 from "./a10.jpg"
 
 
 
@@ -27,4 +73,8 @@ import bloodbank from "./Bloodbank.jpg"
 
 
 
-export { sanjeevaniImg,offerimg,bannerimg,medicineimg,doctorimg,labimg,healthimg,monitorimg,babyimg,painimg,proteinimg,vitaminimg,skincareimg,ayurvedicimg,Nutritiousimg,summerimg,Docimg,ambulance,bloodbank};
+export { sanjeevaniImg,offerimg,bannerimg,medicineimg,doctorimg,labimg,healthimg,monitorimg,babyimg,painimg,proteinimg,vitaminimg,skincareimg,ayurvedicimg,Nutritiousimg,summerimg,Docimg,ambulance,bloodbank,p1, p2, p3, p4, p5, p6, p7, p8, p9, p10,
+a1, a2, a3, a4, a5, a6, a7, a8, a9, a10,
+h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12, h13, h14, h15, h16,
+b1, b2, b3, b4, b5, b6, b7, b8, b9, b10
+};

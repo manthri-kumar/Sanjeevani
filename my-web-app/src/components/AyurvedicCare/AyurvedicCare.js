171b2fda809a83f7b48d30418e51d15e16892db8
ReddyEswarK126 <katarureddyeswar@gmail.com>
2025-11-13 16:46:21 +0530

    Updated by added photos in Ayurvedic Care Page

--- my-web-app/src/components/AyurvedicCare/AyurvedicCare.js
+++ my-web-app/src/components/AyurvedicCare/AyurvedicCare.js
@@ -1,6 +1,6 @@
 import React, { useState, useMemo } from "react";
 import "./AyurvedicCare.css";
-import { sanjeevaniImg, offerimg } from "../../assets"; // ✅ use your own banner and logo paths
+import { sanjeevaniImg, offerimg ,bloodbank,a1, a2, a3, a4, a5, a6, a7, a8, a9, a10} from "../../assets"; // ✅ use your own banner and logo paths
 
 const allProducts = [
   {
@@ -10,7 +10,7 @@ const allProducts = [
     price: 349,
     mrp: 499,
     rating: 4.6,
-    img: "https://images.apollo247.com/pub/media/catalog/product/d/a/dabur_chyawanprash_1kg.jpg",
+    img: a1,
   },
   {
     id: 2,
@@ -19,7 +19,7 @@ const allProducts = [
     price: 199,
     mrp: 230,
     rating: 4.5,
-    img: "https://images.apollo247.com/pub/media/catalog/product/h/i/himalaya_ashwagandha.jpg",
+    img: a2,
   },
   {
     id: 3,
@@ -28,7 +28,7 @@ const allProducts = [
     price: 45,
     mrp: 48,
     rating: 4.4,
-    img: "https://images.apollo247.com/pub/media/catalog/product/z/a/zandu_balm_25ml.jpg",
+    img: a3,
   },
   {
     id: 4,
@@ -37,7 +37,7 @@ const allProducts = [
     price: 70,
     mrp: 85,
     rating: 4.3,
-    img: "https://images.apollo247.com/pub/media/catalog/product/b/a/baidyanath_triphala_churna_100g.jpg",
+    img: a4,
   },
   {
     id: 5,
@@ -46,7 +46,7 @@ const allProducts = [
     price: 95,
     mrp: 120,
     rating: 4.2,
-    img: "https://images.apollo247.com/pub/media/catalog/product/p/a/patanjali_giloy_ghanvati.jpg",
+    img: a5,
   },
   {
     id: 6,
@@ -55,7 +55,7 @@ const allProducts = [
     price: 160,
     mrp: 195,
     rating: 4.4,
-    img: "https://images.apollo247.com/pub/media/catalog/product/z/a/zandu_pancharishta_650ml.jpg",
+    img: a6,
   },
   {
     id: 7,
@@ -64,7 +64,7 @@ const allProducts = [
     price: 155,
     mrp: 180,
     rating: 4.7,
-    img: "https://images.apollo247.com/pub/media/catalog/product/h/i/himalaya_liv52_100tabs.jpg",
+    img: a7,
   },
   {
     id: 8,
@@ -73,7 +73,7 @@ const allProducts = [
     price: 175,
     mrp: 199,
     rating: 4.8,
-    img: "https://images.apollo247.com/pub/media/catalog/product/d/a/dabur_honey_500g.jpg",
+    img: a8,
   },
   {
     id: 9,
@@ -82,7 +82,7 @@ const allProducts = [
     price: 95,
     mrp: 110,
     rating: 4.3,
-    img: "https://images.apollo247.com/pub/media/catalog/product/p/a/patanjali_aloe_vera_gel_150ml.jpg",
+    img: a9,
   },
   {
     id: 10,
@@ -91,9 +91,8 @@ const allProducts = [
     price: 425,
     mrp: 520,
     rating: 4.5,
-    img: "https://images.apollo247.com/pub/media/catalog/product/b/a/baidyanath_kesari_kalp_1kg.jpg",
-  },
-  
+    img: a10,
+  }
 ];
 
 
